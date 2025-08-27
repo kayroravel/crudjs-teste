5e80ac839eab411c477aba75c0dc3b54b2c44b1f
kayroravel <kayroravel@hotmail.com>
2025-08-27 00:55:22 -0300

    Melhorando o db.js

--- backend/bd.js
+++ backend/bd.js
@@ -1,8 +1,19 @@
-const {Pool} = require('pg');
+const { Pool } = require('pg');
+
+// Carrega variáveis de ambiente
 require('dotenv').config();
 
+// Cria o pool de conexões
 const pool = new Pool({
-    connectionString : process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
+console.log(rocess.env.DATABASE_URL)
+
 module.exports = pool;
+// Exporta o pool para uso em outros arquivos
+// module.exports = {
+//   query: (text, params) => pool.query(text, params),
+//   pool,
+// };
